e37f030f06cf901358282dd69c0679f4e53b9b00
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-03-09 00:05:21 +0100

    (leanr_algebra.py) fix it

--- leanr_algebra.py
+++ leanr_algebra.py
@@ -1,14 +1,13 @@
-#!/usr/bin/env python3
 import torch
 import torch.nn as nn
-
-from plot import plot_predictions
 
 
 class LeanrAlgebra(nn.Module):
     def __init__(self):
-        super().__init
-        delf.a = nn.Parameter(torch.randn(1, dtype=tensor.float), requires_grad=True)
-        delf.b = nn.Parameter(torch.randn(1, dtype=tensor.float), requires_grad=True)
-    def forward(self, x:torch.Tensor) -> torch.Tensor:
+        super().__init__()
+        self.a = nn.Parameter(torch.randn(1, dtype=torch.float), requires_grad=True)
+        self.b = nn.Parameter(torch.randn(1, dtype=torch.float), requires_grad=True)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.a * x + self.b
+
