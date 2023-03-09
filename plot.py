4cfb80914ade158944d599d2004071355c87fea0
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-03-09 12:28:51 +0100

    (plot.py) make it simple

--- plot.py
+++ plot.py
@@ -2,31 +2,18 @@ import matplotlib.pyplot as plt
 
 
 def plot_predictions(
-    train_data=None,
-    train_labels=None,
-    test_data=None,
-    test_labels=None,
-    predictions=None,
+    X_train, y_train, X_test, y_test, predictions=None
 ):
-    if train_data is None:
-        train_data = X_train
-    if train_labels is None:
-        train_labels = y_train
-    if test_data is None:
-        test_data = X_test
-    if test_labels is None:
-        test_labels = y_test
-
     """
     Plots training data, test data and compares predictions.
     """
     plt.figure(figsize=(10, 7))
-    plt.scatter(train_data, train_labels, c="b", s=4, label="Training data")
-    plt.scatter(test_data, test_labels, c="g", s=4, label="Testing data")
+    plt.scatter(X_train, y_train, c="b", s=4, label="Training data")
+    plt.scatter(X_test, y_test, c="g", s=4, label="Testing data")
     if predictions is not None:
         plt.scatter(
-            test_data.detach().numpy(),
-            predictions.detach().numpy(),
+            X_test,
+            predictions,
             c="r",
             s=4,
             label="Predictions",
