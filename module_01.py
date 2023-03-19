d8ff56d0a0ed76f96932847986d27c5843fd29ed
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-03-19 11:14:14 +0000

    (module_01.py) up

--- module_01.py
+++ module_01.py
@@ -5,6 +5,7 @@ import torch
 import torch.nn as nn
 
 from leanr_algebra import LeanrAlgebra
+from plot import plot_predictions
 
 X = torch.arange(0, 1, 0.02)
 y = 0.7 * X + 0.3
@@ -21,7 +22,7 @@ module_0 = LeanrAlgebra()
 loss_fn = nn.L1Loss()
 optimizer = torch.optim.SGD(params=module_0.parameters(), lr=0.01)
 
-epochs = 10_000
+epochs = 1_000
 for epoch in range(epochs):
     # Training
     torch.manual_seed(42)
@@ -36,6 +37,6 @@ with torch.inference_mode():
     y_pred_test = module_0(X_test)
 
 
-# plot_predictions(X_train, y_train, X_test, y_test, predictions=y_pred_test)
+plot_predictions(X_train, y_train, X_test, y_test, predictions=y_pred_test)
 print(module_0.state_dict())
 os._exit(0)
