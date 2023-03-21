385667fad696ddaad3c27eea95a55c0e4ef4dd46
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-03-21 12:50:52 +0100

    (module_02.py) separate load from training+saving model

--- module_02.py
+++ module_02.py
@@ -55,14 +55,3 @@ with torch.inference_mode():
 
 
 PATH_MODEL = model_path("02_pytorch.pth")
-
-# torch.save(model.state_dict(), PATH_MODEL)
-
-# from leanr_algebraV2 import LeanrAlgebraV2
-# model_eval = LeanrAlgebraV2()
-# model_eval.load_state_dict(torch.load(PATH_MODEL))
-# model_eval.eval()
-# with torch.inference_mode():
-#     y2=model_eval(X_test)
-#
-# plot_predictions(X_train, y_train, X_test, y_test, predictions=y2)
